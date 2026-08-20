b3c5f5eab6916bb46bf25345daa67523ad50f1f5
gcaguilar <38892827+gcaguilar@users.noreply.github.com>
2026-08-20 18:44:33 +0200

    chore(release): bump to 0.22.16

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -90,8 +90,8 @@ android {
     applicationId = "com.gcaguilar.biciradar"
     minSdk = 29
     targetSdk = 36
-    versionCode = 29570201
-    versionName = "0.22.15"
+    versionCode = 29570202
+    versionName = "0.22.16"
     manifestPlaceholders["googleMapsApiKey"] = googleMapsApiKey.get()
   }
 
--- wearApp/build.gradle.kts
+++ wearApp/build.gradle.kts
@@ -82,8 +82,8 @@ android {
     applicationId = wearApplicationId
     minSdk = 30
     targetSdk = 36
-    versionCode = 29570202
-    versionName = "0.22.15"
+    versionCode = 29570203
+    versionName = "0.22.16"
   }
 
   flavorDimensions += "tier"
